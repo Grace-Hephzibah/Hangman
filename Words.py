2996779b350cf5cb3835fced497af90a61585326
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-02-02 08:51:06 +0530

    Add files via upload

---
+++ Words.py
@@ -0,0 +1,3 @@
+words = '''ant bat chick hen duck elephant bear lion tiger cow bull crow whale
+frog toad moose deer snake snail swan salmon parrot cuckoo raven eagle horse
+falcon turkey bison'''.split()
